e817202af1009aa4ee11610c9f551251edbfaf8a
Rebeca Méndez <rebeca.mendez@getmanfred.com>
2023-10-21 23:02:57 +0200

    feat(api): add class validator

--- src/api/src/main.setup.ts
+++ src/api/src/main.setup.ts
@@ -1,5 +1,6 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 export const setupNestApp = (app: INestApplication): void => {
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableShutdownHooks();
 };
